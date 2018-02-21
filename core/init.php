57d66498a0a58df895b75d5fab1e4ee02e855f0b
Sebi <sebastian.ion@betarena.ro>
2018-02-21 10:58:21 +0200

    am pus numele bazei de date inpoi in tutorial

--- core/init.php
+++ core/init.php
@@ -1,5 +1,5 @@
 <?php
-$db = mysqli_connect ('127.0.0.1','root', '', 'ecommerce');
+$db = mysqli_connect ('127.0.0.1','root', '', 'tutorial');
 
 if(mysqli_connect_errno()){
     echo 'Database connect failed with following errors : '. mysqli_connect_error();
